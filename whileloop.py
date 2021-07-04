6412c6725283cc82e15660fa524f2e2f05284464
samar pratap yadav <66524944+SAMARpratapYADAV@users.noreply.github.com>
2021-07-04 20:05:15 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,12 @@
+x = "samar"
+x = int()
+y = input("enter the any character to be found in samar    ")
+z = print('''it gives the ans that the 
+character has been found''')
+
+while x <= 5:
+    if x == y:
+        print(z)
+    else:
+        print("not found")
+x += 1
